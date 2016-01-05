5b492b1eaf20726f61e71ea7a1245f95d61c3d18
Ken Wilcox <wilcoxk@medamerica.com>
2016-01-05 14:38:49 -0700

    one numbers test passes

--- numbers.js
+++ numbers.js
@@ -2,18 +2,18 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+      
   },
 
   base10: function(str) {
-
+      return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+      
   },
 
   multiply: function(a, b) {
-
+      
   }
 };
